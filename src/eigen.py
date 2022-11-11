bc31f66c6864caf3cec57d24aad9c78e78f1c769
tobisns <13521090@std.stei.itb.ac.id>
2022-11-12 01:13:43 +0700

    added eige vector not precise

--- src/eigen.py
+++ src/eigen.py
@@ -21,10 +21,8 @@ def givenRot(a):
     n = len(a)
     for j in range(n-2):
         for i in range(j+2,n):
-            gRot = np.zeros(shape=[n,n])
-            for k in range(n):
-                gRot[k][k] = 1
-            alph = np.sqrt(a[i][j]**2+a[j+1][j]**2)
+            gRot = np.identity(n)
+            alph = np.sqrt(np.power(a[i][j],2)+np.power(a[j+1][j],2))
             c = a[j+1][j]/alph
             s = a[i][j]/alph
             gRot[i][i] = c
@@ -32,7 +30,9 @@ def givenRot(a):
             gRot[i][j+1] = -s
             gRot[j+1][i] = s
             a = np.matmul(np.matmul(gRot,a),gRot.transpose())
-    return a    
+    return a  
+
+  
 
 # def QR_Decomposition(A):
 #     n, m = A.shape # get the shape of A
@@ -74,19 +74,21 @@ def givenRot(a):
 
 
 # a = np.random.rand(8,8)
-# a = np.random.randint(99,size=(100,100))
-a = np.random.randint(100,size=(100,100))
+# # a = np.random.randint(99,size=(100,100))
+# a = np.random.randint(100,size=(100,100))
 
 def eig(a,maxiter=1000):
-    a = givenRot(a)
+    n = len(a)
+    aTemp = givenRot(a)
+    # aTemp = a
+    eigenvec = np.identity(n)
     for i in range(maxiter):
-        b, c = qr(a)
-        a = np.matmul(c,b)
-    eigenval = np.empty(len(a))
-    for i in range(len(a)):
-        eigenval[i] = a[i][i]
+        q, r = qr(aTemp)
+        aTemp = np.matmul(r,q)
+        eigenvec = np.matmul(eigenvec,q)
+    eigenval = np.diag(aTemp)
     
-    return eigenval
+    return eigenval, eigenvec
 
 # b,c = qr(a)
 # print(np.matmul(b,c))
@@ -96,33 +98,38 @@ def eig(a,maxiter=1000):
 
 # print(po)
 
-b = np.random.rand(5,5)
+# b = np.random.rand(5,5)
 
-# gRot = np.zeros(shape=[3,3])
-# for i in range(3):
-#     gRot[i][i] = 1
+# # gRot = np.zeros(shape=[3,3])
+# # for i in range(3):
+# #     gRot[i][i] = 1
 
-# alph = np.sqrt(b[1][0]**2+b[0][0]**2)
-# c = b[0][0]*alph/(b[1][0]**2+b[0][0]**2)
-# s = np.sqrt(1-c**2)
-# print(c,s)
-# gRot[1][1] = c
-# gRot[0][0] = c
-# gRot[1][0] = -s
-# gRot[0][1] = s
-# print(gRot)
+# # alph = np.sqrt(b[1][0]**2+b[0][0]**2)
+# # c = b[0][0]*alph/(b[1][0]**2+b[0][0]**2)
+# # s = np.sqrt(1-c**2)
+# # print(c,s)
+# # gRot[1][1] = c
+# # gRot[0][0] = c
+# # gRot[1][0] = -s
+# # gRot[0][1] = s
+# # print(gRot)
 
-a = np.array([[80,4,22,1,40,63,8],[24,41,6,53,2,41,2],
-                [9,24,6,-38,3,27,19],[-23,-29,-54,29,11,67,47],
-                [1,82,10,12,8,23,28],[-12,43,3,-22,72,21,36],
-                [11,12,1,35,1,86,90]])
+# a = np.array([[80,4,22,1,40,63,8],[24,41,6,53,2,41,2],
+#                 [9,24,6,-38,3,27,19],[-23,-29,-54,29,11,67,47],
+#                 [1,82,10,12,8,23,28],[-12,43,3,-22,72,21,36],
+#                 [11,12,1,35,1,86,90]])
 
-# print(np.matmul(gRot,b))
-print(eig(a,100))
-print(eig(a,500))
-print(eig(a,700))
-print(eig(a,10000))
-s,t = la.eig(a)
-print((np.real(s)))
+# # print(np.matmul(gRot,b))
+# # print(eig(a,100))
+# # print(eig(a,500))
+# # print(eig(a,700))
+# # print(eig(a,10000))
 
-# print(givenRot(b))
+# f,g = eig(a)
+
+# s,t = la.eig(a)
+# print(t)
+# print()
+# print(g)
+
+# # print(givenRot(b))
