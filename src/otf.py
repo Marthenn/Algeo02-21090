4e9b45620ffe4231181eef8f555d10841990a666
zidane <13521163@mahasiswa.itb.ac.id>
2022-11-16 10:41:44 +0700

    update phase 1 training to try compressed covariance matrix

--- src/otf.py
+++ src/otf.py
@@ -1,7 +1,10 @@
 import os
+import time
 
 import numpy as np
 import cv2
+import eigen
+
 
 def get_pict_array(url):
     array = []
@@ -13,10 +16,10 @@ def get_pict_array(url):
 
         print("crawling:", dirpath)
 
-        for i, file in enumerate(filenames):
+        for j, file in enumerate(filenames):
             image = cv2.imread(os.path.join(dirpath, file), cv2.IMREAD_GRAYSCALE)
             # normalize pixels values to floating point range [0..1] inclusive
-            image = cv2.resize(image, (100, 100), interpolation=cv2.INTER_AREA) / 255
+            image = cv2.resize(image, (50, 50), interpolation=cv2.INTER_AREA) / 255
 
             if first:
                 first = False
@@ -24,15 +27,22 @@ def get_pict_array(url):
             else:
                 array = np.vstack((array, image.flatten()))
 
-            if i == 20:
-                break
-
             break
 
     return array
 
 
 def get_mean_vspace(array):
+    sh = array.shape
+
+    # if array has length 1, we'll skip calculating the sum since it'll give a one element arr
+    try:
+        if sh[1] == 1:
+            return array
+
+    except IndexError:
+        return array
+
     return np.sum(array, axis=0) / array.shape[0]
 
 
@@ -42,3 +52,28 @@ def get_mean_diff_array(array, mean_array):
 
 def get_coeff_array(array):
     return np.dot(array.T, array) / array.shape[0]
+
+
+def get_compressed_cov_array(array):
+    return np.dot(array, array.T)
+
+
+def get_cov_from_file(url):
+    pict_arr = get_pict_array(url)
+
+    return get_compressed_cov_array(get_mean_diff_array(pict_arr, get_mean_vspace(pict_arr)))
+
+# pict_arr = get_pict_array('../test/train')
+# coeff_arr = get_coeff_array(get_mean_diff_array(pict_arr, get_mean_vspace(pict_arr)))
+#
+# vects = eigen.eig(coeff_arr)[1]
+#
+# for i in range(len(vects)):
+#     print("printing face")
+#     face = np.array(vects[i]).reshape(50, 50)
+#     face *= 255
+#     print(face)
+#     cv2.imwrite("/home/zidane/kuliah/Semester 3/IF2123 - Aljabar Linier dan Geometri/mean_face_.jpg",
+#                 face)
+#
+#     break
