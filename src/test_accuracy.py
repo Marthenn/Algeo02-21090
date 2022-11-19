35f1083e3a41c673011214f3697ae2fcf7347716
zidane <13521163@mahasiswa.itb.ac.id>
2022-11-19 07:46:13 +0700

    remove loop from accuracy test

--- src/test_accuracy.py
+++ src/test_accuracy.py
@@ -2,23 +2,24 @@
 import os
 from recog import *
 
-def test_accuracy(multiplier,data, treshold):
-    print("Testing accuracy with multiplier = {}".format(multiplier))
-    folder_path = 'D:/Kuliah/Semester 3/AlGeo/Algeo02-21090/test/val'
+
+def test_accuracy(data, treshold):
+    print("Testing accuracy with treshold = {}".format(treshold))
+    folder_path = '../test/archive(1)'
     true = 0
     total = 0
-    tres = treshold * multiplier
+    tres = treshold
     print("Treshold = {}".format(tres))
     # print("folder path = {}".format(folder_path))
     for i, (dirpath, dirnames, filename) in enumerate(os.walk(folder_path)):
-        if dirpath == folder_path:
-            continue
+
         # print('Processing {}'.format(os.path.basename(os.path.normpath(dirpath))))
         for j, pic in enumerate(filename):
             # print('Processing {}'.format(pic))
-            if pic.endswith(".jpg") or pic.endswith(".png") or pic.endswith(".jpeg"):
+            if pic.endswith(".jpg") or pic.endswith(".png") or pic.endswith(".jpeg") or pic.endswith(".pgm"):
                 res = find_match(os.path.join(dirpath, pic), data, tres)
-                if res != None: 
+                if res != None:
+                    print(os.path.basename(os.path.normpath(dirpath)), res[0], 'path')
                     if res[0] == os.path.basename(os.path.normpath(dirpath)):
                         true += 1
                 total += 1
@@ -34,11 +35,12 @@ def test_accuracy(multiplier,data, treshold):
     #                 if res[0] == os.path.basename(os.path.normpath(dirpath)):
     #                     true += 1
     #             total+=1
-    print("Accuracy: {}".format(true/total))
+    print("true: {} total: {} Accuracy: {}".format(true, total, true / total))
+
 
 if __name__ == '__main__':
-    data = read_from_yml("D:\Kuliah\Semester 3\AlGeo\Algeo02-21090", "db.yml")
-    treshold = get_treshold(data, 1)
-    for i in range(5,105,5):
-        i/=100
-        accuracy = test_accuracy(i,data,treshold)
+    data = read_from_yml("/home/zidane/kuliah/Semester 3/IF2123 - Aljabar Linier dan Geometri/Algeo02-21090", "db.yml")
+    tresh = get_treshold(data)
+    if tresh == 0:
+        tresh = 500
+    test_accuracy(data, tresh)
